e8c378954d024c350feaaf0db709340dd9bea5ce
Andrew Shields <andrew.shields@revature.net>
2021-04-13 17:02:44 -0500

    added components to trainer module that reflect dashboard features & sidenav

---
+++ src/app/trainers/components/batch-data/batch-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-batch-data',
+  templateUrl: './batch-data.component.html',
+  styleUrls: ['./batch-data.component.css']
+})
+export class BatchDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/trainers/trainers.module.ts
+++ src/app/trainers/trainers.module.ts
@@ -7,6 +7,11 @@ import { ClosetabComponent } from './components/closetab/closetab.component';
 import { ExpandtabComponent } from './components/expandtab/expandtab.component';
 import { InfoComponent } from './components/info/info.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { BatchDataComponent } from './components/batch-data/batch-data.component';
+import { ReferencesComponent } from './components/references/references.component';
+import { RepoLinksComponent } from './components/repo-links/repo-links.component';
+import { InviteComponent } from './components/invite/invite.component';
 
 
 @NgModule({
@@ -15,7 +20,12 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     ClosetabComponent,
     ExpandtabComponent,
     InfoComponent,
-    SidenavComponent
+    SidenavComponent,
+    CurriculumComponent,
+    BatchDataComponent,
+    ReferencesComponent,
+    RepoLinksComponent,
+    InviteComponent
   ],
   imports: [
     CommonModule,
