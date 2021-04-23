7ec5654f8e4ac650daa985b5b3aed06e2cff90cb
Jose Rubio <rubioj.m.93@gmail.com>
2021-04-23 17:22:59 -0400

    Updated components: nav and registration

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { LoginResponse } from './../../models/login-response';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'app/services/auth.service';
@@ -9,17 +10,48 @@ import { AuthService } from 'app/services/auth.service';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
-
-  ngOnInit(): void {
-  }
-
+  repsonse!: LoginResponse;
   isLoggedIn = false;
+  isLoggedOut= false;
+
 
   isTrainer = false;
 
   isAssigned = false;
 
   isVerified = false;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+
+
+  loggedIn(){
+    if(sessionStorage.getItem("token")!== null){
+      this.isLoggedIn = true;
+      // this.isLoggedOut = true;
+    }
+    else{
+      this.isLoggedIn = false;
+      // this.isLoggedOut = false;
+
+    }
+
+  }
+
+  clearSessionStorage(){
+    if(sessionStorage.getItem("token")===null){
+      // this.isLoggedOut = false;
+      this.isLoggedIn = true;
+    }
+    else{
+      this.isLoggedIn=false;
+      // this.isLoggedOut =true;
+      return sessionStorage.clear();
+    }
+    }
 
 }
