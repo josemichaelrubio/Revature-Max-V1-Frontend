60424a5d32647cd9216bf9def712cd6d1401c122
Matthew Carlson <cmatt97@vt.edu>
2021-05-03 21:47:05 +0000

    Calendar keeps track of topic moving

--- src/app/trainers/components/curriculum/curriculum.component.ts
+++ src/app/trainers/components/curriculum/curriculum.component.ts
@@ -11,16 +11,15 @@ import { TopicService } from 'app/services/topic.service';
 @Component({
   selector: 'app-curriculum',
   templateUrl: './curriculum.component.html',
-  styleUrls: ['./curriculum.component.css']
+  styleUrls: ['./curriculum.component.css'],
 })
 export class CurriculumComponent implements OnInit {
-
   showDayModal: boolean = false;
   showEditButton: boolean = false;
   curriculum: Curriculum[] = [];
   formData!: FormGroup;
   formRemoveData!: FormGroup;
-  day!:string;
+  day!: string;
   topicTitle!: string;
   tagList: Tag[] = [];
   topicList: Topic[] = [];
@@ -29,80 +28,126 @@ export class CurriculumComponent implements OnInit {
   topicIdClick!: string;
   topicTagId!: string;
 
-  events = [
-    { id: "0", title: 'Start Date', date: '2021-03-01', tag: "0" }
-  ];
+  events = [{ id: '0', title: 'Start Date', date: '2021-03-01', tag: '0' }];
 
-  @ViewChild("content") private contentRef!: TemplateRef<Object>;
+  @ViewChild('content') private contentRef!: TemplateRef<Object>;
 
-  constructor(private topicService: TopicService, private router: Router, private curriculumService: CurriculumService) {
-    curriculumService.getCurriculumDays().subscribe((data)=>{
-      this.curriculum=data;
-      for(let curDay of this.curriculum){
-        if(curDay.topics!=null){
-          for(let topic of curDay.topics){
-          this.events.push({ id: `${topic.id}`, title: topic.name, date: curDay.date, tag: `${topic.tag.id}`});
+  constructor(
+    private topicService: TopicService,
+    private router: Router,
+    private curriculumService: CurriculumService
+  ) {
+    curriculumService.getCurriculumDays().subscribe(
+      (data) => {
+        this.curriculum = data;
+        for (let curDay of this.curriculum) {
+          if (curDay.topics != null) {
+            for (let topic of curDay.topics) {
+              this.events.push({
+                id: `${topic.id}`,
+                title: topic.name,
+                date: curDay.date,
+                tag: `${topic.tag.id}`,
+              });
+            }
           }
-          
         }
-        
+        this.calendarOptions.events = this.events;
+      },
+      (err) => console.log(err),
+      () => {
+        console.log('yay curriculum');
       }
-      this.calendarOptions.events = this.events;
-    },
-    (err)=>console.log(err),
-    ()=>{
-      console.log('yay curriculum');
-    });
+    );
 
-    topicService.getAllTopics().subscribe((data)=>this.topicList=data);
-   }
+    topicService.getAllTopics().subscribe((data) => (this.topicList = data));
+  }
 
   ngOnInit(): void {
     this.formData = new FormGroup({
-      topicName: new FormControl("Topic"),
-      eventDay: new FormControl("Day")
+      topicName: new FormControl('Topic'),
+      eventDay: new FormControl('Day'),
     });
     this.formRemoveData = new FormGroup({
       topicName: new FormControl(this.topicNameClick),
-      topicId: new FormControl(this.topicIdClick)
+      topicId: new FormControl(this.topicIdClick),
     });
   }
   handleDateClick(arg: any) {
     // alert('date click! ' + arg.dateStr)
-    this.day = arg.dateStr
+    this.day = arg.dateStr;
   }
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick.bind(this), // bind is important!
     eventClick: this.handleEventClick.bind(this),
+    eventDrop: this.handleEventDrop.bind(this),
     editable: true,
     selectable: true,
     selectMirror: true,
-    weekends: false // initial value
+    weekends: false, // initial value
   };
 
   toggleWeekends() {
-    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    this.calendarOptions.weekends = !this.calendarOptions.weekends; // toggle the boolean!
   }
 
-  handleEventClick(arg:any){
+  handleEventClick(arg: any) {
     this.topicIdClick = arg.event._def.sourceId;
     this.topicNameClick = arg.event._def.title;
     this.topicTagId = arg.event._def.extendedProps.tag;
+    console.log(this.curriculum);
   }
 
-  loadTags(){
-    this.topicService.getAllTags().subscribe((data)=>this.tagList = data);}
+  /**
+   * Updates this.curriculum when a topic is moved from one day to another
+   * @param arg
+   * @returns
+   */
+  handleEventDrop(arg: any) {
+    // get the days the topic is moved from / to
+    const initDay: Curriculum | undefined = this.curriculum.find(
+      (curr) =>
+        curr.date ==
+        arg.oldEvent._instance.range.start.toISOString().split('T')[0]
+    );
+    const destDay: Curriculum | undefined = this.curriculum.find((curr) => {
+      return (
+        curr.date == arg.event._instance.range.start.toISOString().split('T')[0]
+      );
+    });
 
-  loadTopics(){
-    this.topicService.getAllTopics().subscribe((data)=>this.topicList = data);
+    // Move topic from initDay to destDay
+    const movedTopic: Topic | undefined = initDay?.topics.find(
+      (topic) => topic.name == arg.event._def.title
+    );
+    if (!movedTopic) {
+      return;
+    }
+    initDay?.topics.splice(
+      initDay.topics.findIndex((e) => e.id == movedTopic.id)
+    );
+    destDay?.topics.push(movedTopic);
 
+    // console.log(initDay?.topics);
+    // console.log(destDay?.topics);
+
+    // TODO: Mark what days are updated so when we save changes it keeps the number of requests to a minimum
+    // Can add this when getting backend requets set up
   }
 
+  loadTags() {
+    this.topicService.getAllTags().subscribe((data) => (this.tagList = data));
+  }
 
-  saveTopic(topic:any){
+  loadTopics() {
+    this.topicService
+      .getAllTopics()
+      .subscribe((data) => (this.topicList = data));
+  }
+
+  saveTopic(topic: any) {
     this.topicIdClick = topic.id;
   }
-  
 }
