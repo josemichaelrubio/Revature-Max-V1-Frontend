25c9411f30b55b1cc6c0002810fde3b71f04f893
Matthew Carlson <cmatt97@vt.edu>
2021-05-04 13:29:34 -0400

    added QC model

--- src/app/models/batch-day.ts
+++ src/app/models/batch-day.ts
@@ -1,3 +1,4 @@
+import { QC } from './qc';
 import { Quiz } from './quiz';
 import { Topic } from './topic';
 
@@ -7,18 +8,21 @@ export class BatchDay {
   date: string;
   topics: Topic[];
   quiz: Quiz;
+  qc: QC;
 
   constructor(
     _id: number,
     _batchId: number,
     _date: string,
     _topics: Topic[],
-    _quiz: Quiz
+    _quiz: Quiz,
+    _qc: QC
   ) {
     this.id = _id;
     this.batchId = _batchId;
     this.date = _date;
     this.topics = _topics;
     this.quiz = _quiz;
+    this.qc = _qc;
   }
 }
